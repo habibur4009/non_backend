d10ffec57ead25c6f5970ff16565dd875b3bc4ab
Md Habibur Rahman <58021940+habibur4009@users.noreply.github.com>
2026-05-02 21:47:58 +0600

    Restore Django settings with initial configuration

--- ninProject/settings.py
+++ ninProject/settings.py
@@ -13,14 +13,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from datetime import timedelta
 import os
 from pathlib import Path
-import firebase_admin
-from firebase_admin import credentials
 import dj_database_url
 
-cred = credentials.Certificate(
-    "ninUser/topper-46b7d.json"
-)
-firebase_admin.initialize_app(cred)
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
